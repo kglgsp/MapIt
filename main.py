894499ca31394970c729a065261ba5c896960197
Katherine Legaspi <klega002@ucr.edu>
2019-03-05 22:57:21 -0800

    setup api keys

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-import json
 import tweepy
+import sys
+import time
+import json
+
+consumer_token = '33vZHXkQiOjJMdhybWEVyry2T'
+consumer_secret = 'lqd1cG4Jm28xh3c8NqANvar0f8duc9eGMx1D5GbDCnOj8zu7iE'
+
+access_token = '27821157-la768cgDthKzjwej8K2bq3ZxjILX5hXu5BZOE4R47'
+access_secret = 'YiymUJfhAe2JDjyKMpKxEVSbtr20ereAbZJVqG4QyZTcF'
+
+auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+class myStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on-data disconnects the stream
+        return False
+
+    def on_timeout(self):
+        time.sleep(60)
+        return
+
+    def on_limit(self, status_code):
+        return False
+
+
