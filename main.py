d557e696af6000cf0278f6db796261b88a24a5bf
root <root@LAPTOP-I6M0MNH4.localdomain>
2019-03-09 23:10:06 -0800

    created buffer for twitter stream, connected them to dataframe for easier computation

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import tweepy
 import time
 import json
 import geocoder
+
+
+import pandas as pd
+
 consumer_token = '33vZHXkQiOjJMdhybWEVyry2T'
 consumer_secret = 'lqd1cG4Jm28xh3c8NqANvar0f8duc9eGMx1D5GbDCnOj8zu7iE'
 
@@ -13,17 +17,78 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
+
+
 class StreamListener(tweepy.StreamListener):
     def __init__(self, api):
         self.api = api
         super(tweepy.StreamListener, self).__init__()
-        tweet = []
+
+
         self.tweet_file = open('tweets.json', 'a')
+        self.tweetList = []
 
     def on_data(self, tweet):
         #self.tweet_file.append(json.loads(tweet))
-        print(tweet)
-        self.tweet_file.write(str(tweet))
+        print("===TWEET TEXT===")
+        theTweet = json.loads(tweet)
+
+        try:
+            importantColumns = [theTweet['text' ] ,
+                    theTweet['id' ],
+                    theTweet['entities'][u'urls'] ,
+                    theTweet['favorite_count'], 
+                    theTweet['retweet_count'],
+                    theTweet['timestamp_ms'] ,
+                    theTweet['user'][u'profile_image_url'], 
+                    theTweet['user'][u'screen_name'], 
+                    theTweet ['extended_text'] , 
+                    theTweet['created_at'] , 
+                    theTweet['place'][u'full_name'] , 
+                    theTweet['place'][u'bounding_box'] ] 
+        except KeyError:
+            importantColumns = [theTweet['text' ] , 
+                    theTweet['id' ], 
+                    theTweet['entities'][u'urls'] , 
+                    theTweet['favorite_count'], 
+                    theTweet['retweet_count'], 
+                    theTweet['timestamp_ms'] , 
+                    theTweet['user'][u'profile_image_url'], 
+                    theTweet['user'][u'screen_name'], 
+                    None , 
+                    theTweet['created_at'], 
+                    theTweet['place'][u'full_name'] , 
+                    theTweet['place'][u'bounding_box'] ] 
+        
+        self.tweetList.append(importantColumns)
+        
+        if len(self.tweetList) >= 3:
+            
+            self.tweetFrame = pd.DataFrame(self.tweetList,columns =  ["[text]",
+                "[id]",
+                "urls",
+                "[favorite_count]",
+                "[retweet_count]",
+                "[timestamp_ms]",
+                "[user][u'profile_image_url']",
+                "[user][u'screen_name']",
+                "[extended_text]",
+                "[created_at]",
+                "[place][u'full_name]",
+                "[place][u'bounding_box']"]) 
+       
+
+            for index, row in self.tweetFrame.iterrows():
+                print("row url: ", row['urls'])
+                if len(row['urls']) > 0:
+                    print("The tweet " + str(row['[id]']) + " is not null")
+                    
+                
+            self.tweetList = []
+
+            print("===Dataframe===")
+            print(self.tweetFrame)
+        #self.tweet_file.write(str(tweet))
 
     def on_error(self, status_code):
         if status == 420:
@@ -43,11 +108,12 @@ class StreamListener(tweepy.StreamListener):
 
 
 def main():
-    
 
     g = geocoder.ip('me')
     currentLocation = g.latlng
     LOCATIONS = [currentLocation[1]-1.5,currentLocation[0]-1.5,currentLocation[1]+1.5,currentLocation[0]+1.5]
+
+
 
     stream_listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True))
     stream = tweepy.Stream(auth=auth, listener=stream_listener)
